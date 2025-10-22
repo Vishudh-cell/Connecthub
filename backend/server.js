4a8b2d9d5727f73c170c0467dd292272389239f6
Vishudh-cell <vishudh.24bcs10162@sst.scaler.com>
2025-10-22 16:28:13 +0530

    Revert "Fix port conflict by switching to 5001 and update .env"
    This reverts commit ea468036011e15d5d9be8622cb54a88ed6c564f1.

--- backend/server.js
+++ backend/server.js
@@ -19,5 +19,3 @@ app.use('/api/users', userRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-console.log("Mongo URL:", process.env.MONGO_URL);
